e6d86affe9db39c1d42f1d933ae9369998265045
Oleksandr Kovbasiuk <kovbasiuk.o@gmail.com>
2019-11-11 01:35:54 +0200

    Added new Utility class and ToBitmapSource conversion

---
+++ DominantColoursSearch_Solution/DominantColoursSearch/Utility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+using NLog;
+
+namespace DominantColoursSearch
+{
+    public static class Utility
+    {
+        private static Logger Logger { get; } = LogManager.GetCurrentClassLogger();
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        private static extern bool DeleteObject(IntPtr hObject);
+
+        public static BitmapSource ToBitmapSource(System.Drawing.Bitmap bitmap)
+        {
+            BitmapSource result = null;
+
+            try
+            {
+                IntPtr ptr = bitmap.GetHbitmap();
+
+                try
+                {
+                    result = Imaging.CreateBitmapSourceFromHBitmap(
+                                 ptr,
+                                 IntPtr.Zero,
+                                 Int32Rect.Empty,
+                                 BitmapSizeOptions.FromEmptyOptions());
+                }
+                finally
+                {
+                    DeleteObject(ptr);
+                    bitmap.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("ToBitmapSource");
+
+                throw ex;
+            }
+
+
+            return result;
+        }
+    }
+}
