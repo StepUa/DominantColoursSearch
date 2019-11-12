b26c2084bd65f3398932aa2f0279b2dccd5a9c03
Oleksandr Kovbasiuk <kovbasiuk.o@gmail.com>
2019-11-12 02:57:50 +0200

    Partial implementation of UserControl that stores analysis information

---
+++ DominantColoursSearch_Solution/DominantColoursSearch/Controls/ImageResultInfoControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DominantColoursSearch.Controls.ViewModels;
+
+namespace DominantColoursSearch.Controls
+{
+    /// <summary>
+    /// Interaction logic for ImageResultInfoControl.xaml
+    /// </summary>
+    public partial class ImageResultInfoControl : UserControl
+    {
+        public ImageResultInfoControl()
+        {
+            InitializeComponent();
+        }
+
+        public ImageResultInfoControlViewModel ViewModel { get; set; }
+    }
+}
---
+++ DominantColoursSearch_Solution/DominantColoursSearch/Controls/ViewModels/ImageResultInfoControlViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using DominantColoursSearch.CustomClasses;
+
+namespace DominantColoursSearch.Controls.ViewModels
+{
+    public class ImageResultInfoControlViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<PictureDominantColorInfoItem> _imageResultInfoCollection;
+        public ObservableCollection<PictureDominantColorInfoItem> ImageResultInfoCollection
+        {
+            get => this._imageResultInfoCollection;
+            set
+            {
+                if (Object.ReferenceEquals(this._imageResultInfoCollection, value))
+                {
+                    return;
+                }
+
+                this._imageResultInfoCollection = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
