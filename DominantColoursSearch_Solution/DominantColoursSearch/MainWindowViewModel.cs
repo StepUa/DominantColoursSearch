09e07a979a9c424386d681f84e4a225986fc668f
Oleksandr Kovbasiuk <kovbasiuk.o@gmail.com>
2019-12-03 01:37:31 +0200

    New collection for Analyzers and property for file name

--- DominantColoursSearch_Solution/DominantColoursSearch/MainWindowViewModel.cs
+++ DominantColoursSearch_Solution/DominantColoursSearch/MainWindowViewModel.cs
@@ -18,6 +18,22 @@ namespace DominantColoursSearch
 
         private List<Task> TasksWithAnalyzers;
 
+        private string[] _filePaths;
+        public string[] FilePaths
+        {
+            get => this._filePaths;
+            set
+            {
+                if (Object.ReferenceEquals(this._filePaths, value))
+                {
+                    return;
+                }
+
+                this._filePaths = value;
+                RaisePropertyChanged();
+            }
+        }
+
         private string[] _fileNames;
         public string[] FileNames
         {
@@ -55,24 +71,47 @@ namespace DominantColoursSearch
             set => SetProperty(ref this._statusCurrentAnalysisTime, value);
         }
 
+        private int _selectedAnalyzerUniqeIndex;
+        public int SelectedAnalyzerUniqeIndex
+        {
+            get => this._selectedAnalyzerUniqeIndex;
+            set => SetProperty(ref this._selectedAnalyzerUniqeIndex, value);
+        }
+
         public bool IsViewModelInitialized { get; private set; }
 
-        private DominantColoursAnalyzer[] Analyzers { get; set; }
-
-        public void InitializeViewModel(string[] fileNamesInput, EventHandler handler)
+        private ObservableCollection<DominantColoursAnalyzer> _analyzers;
+        public ObservableCollection<DominantColoursAnalyzer> Analyzers
         {
-            if (fileNamesInput == null)
+            get => this._analyzers;
+            set
+            {
+                if (Object.ReferenceEquals(this._analyzers, value))
+                {
+                    return;
+                }
+
+                this._analyzers = value;
+
+                RaisePropertyChanged();
+            }
+        }
+
+        public void InitializeViewModel(string[] filePaths, string[] fileNames, EventHandler handler)
+        {
+            if (filePaths == null)
             {
                 return;
             }
 
-            this.FileNames = fileNamesInput;
+            this.FilePaths = filePaths;
+            this.FileNames = fileNames;
 
-            this.Analyzers = new DominantColoursAnalyzer[this.FileNames.Length];
-            this.TasksWithAnalyzers = new List<Task>(this.Analyzers.Length);
-            for (int i = 0; i < this.Analyzers.Length; i++)
+            this.Analyzers = new ObservableCollection<DominantColoursAnalyzer>();
+            this.TasksWithAnalyzers = new List<Task>(this.FilePaths.Length);
+            for (int i = 0; i < this.FilePaths.Length; i++)
             {
-                this.Analyzers[i] = new DominantColoursAnalyzer(this.FileNames[i], i);
+                this.Analyzers.Add(new DominantColoursAnalyzer(this.FilePaths[i], this.FileNames[i], i));
 
                 this.Analyzers[i].AnalysisCompleteEvent -= handler;
                 this.Analyzers[i].AnalysisCompleteEvent += handler;
@@ -85,7 +124,7 @@ namespace DominantColoursSearch
         {
             try
             {
-                for (int i = 0; i < this.Analyzers.Length; i++)
+                for (int i = 0; i < this.Analyzers.Count; i++)
                 {
                     int index = i;
                     this.TasksWithAnalyzers.Add(Task.Run(() => this.Analyzers[index].AnalysisFunction()));
