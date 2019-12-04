a817f9002f35e05fccf47eeed31f12003fa94e38
Oleksandr Kovbasiuk <kovbasiuk.o@gmail.com>
2019-12-04 05:02:24 +0200

    New UI logic

--- DominantColoursSearch_Solution/DominantColoursSearch/Windows/DatabaseInteraction/DatabaseInteractionWindowViewModel.cs
+++ DominantColoursSearch_Solution/DominantColoursSearch/Windows/DatabaseInteraction/DatabaseInteractionWindowViewModel.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Prism.Mvvm;
 
 namespace DominantColoursSearch.Windows.DatabaseInteraction
 {
-    public class DatabaseInteractionWindowViewModel
+    public class DatabaseInteractionWindowViewModel : BindableBase
     {
+        private BitmapSource _selectedImage;
+        public BitmapSource SelectedImage
+        {
+            get { return _selectedImage; }
+            set
+            {
+                if (Object.ReferenceEquals(this._selectedImage, value))
+                {
+                    return;
+                }
+
+                this._selectedImage = value;
+
+                RaisePropertyChanged();
+            }
+        }
+
+        private int _selectedImageIndex;
+        public int SelectedImageIndex
+        {
+            get { return _selectedImageIndex; }
+            set { SetProperty(ref this._selectedImageIndex, value); }
+        }
+
+
+        public void LoadImage(string fullPath)
+        {
+            if (!File.Exists(fullPath))
+            {
+                return;
+            }
+
+            using (var sourceImage = new Image<Bgr, Byte>(fullPath))
+            {
+                this.SelectedImage = Utility.ToBitmapSource(sourceImage.ToBitmap());
+            }
+        }
     }
 }
