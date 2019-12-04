518d5dedd85b1b2bebf5bb77dfcdbfed18650a0d
Oleksandr Kovbasiuk <kovbasiuk.o@gmail.com>
2019-12-04 05:02:14 +0200

    New UI logic

--- DominantColoursSearch_Solution/DominantColoursSearch/Windows/Options/OptionsWindowViewModel.cs
+++ DominantColoursSearch_Solution/DominantColoursSearch/Windows/Options/OptionsWindowViewModel.cs
@@ -8,5 +8,17 @@ namespace DominantColoursSearch.Windows.Options
 {
     public class OptionsWindowViewModel
     {
+        public OptionsWindowViewModel()
+        {
+            this.NumberOfClusters = new int[] { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+            this.ImageResizintScale = new double[] { 100, 67, 50, 40, 33.3, 28.6, 25 };
+            this.IsSensitivityModeToBrightColorsEnabled = false;
+            this.LanguageList = new string[] { "English", "Ukrainian" };
+        }
+
+        public int[] NumberOfClusters { get; set; }
+        public double[] ImageResizintScale { get; set; }
+        public bool IsSensitivityModeToBrightColorsEnabled { get; set; }
+        public string[] LanguageList { get; set; }
     }
 }
